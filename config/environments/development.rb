b0133cf2590419f38b869450aace32852e7f4acb
BelovG <white@sarov.info>
2014-05-25 22:29:35 +0400

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,5 @@ AdvertisingPlatform::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.cache_store = :dalli_store
+  #config.cache_store = :dalli_store
 end
